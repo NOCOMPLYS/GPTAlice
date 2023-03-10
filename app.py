907794084a36ae7bef705896cf15badc6092a3ab
NOCOMPLYS <shoveit808@gmail.com>
2023-03-10 20:59:55 +0300

    r

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG)
 
-# Set up your OpenAI API credentials
-openai.api_key = "YOUR_API_KEY"
-
 # Define your Flask route
 @app.route('/', methods=['POST'])
 def resp():
@@ -32,7 +29,8 @@ def hw():
 app.run(debug=True, host='0.0.0.0', port=5000, ssl_context="adhoc")
 
 
-
+# Set up your OpenAI API credentials
+openai.api_key = "YOUR_API_KEY"
 
 def generate_response(prompt):
     response = openai.Completion.create(
