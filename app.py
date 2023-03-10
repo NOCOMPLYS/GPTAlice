1d9465638f2ecd788560ffd65ff57cdf59480b55
NOCOMPLYS <shoveit808@gmail.com>
2023-03-10 17:46:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def generate_response(prompt):
 
     return response.choices[0].text.strip()
 
-app.run(debug=True, host='0.0.0.0', port=5000)
+app.run(debug=True, host='0.0.0.0', port=5000, ssl_context="adhoc")
