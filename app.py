26d1552047d80a3d4f269c88386322503ceea5fc
NOCOMPLYS <shoveit808@gmail.com>
2023-03-11 19:49:41 +0300

    Update app.pu

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def resp():
         }
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=('/root/GPTAlice/cert.pem', '/root/GPTAlice/key.pem'), cert='adhoc')
+    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=('/root/GPTAlice/cert.pem', '/root/GPTAlice/key.pem'))
