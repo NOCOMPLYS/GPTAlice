3ea88dfd92f0580e30d7928737b3afd40a783d1b
Nikolas Miroshnikov <shoveit808@gmail.com>
2023-03-11 12:07:34 +0300

    Update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import openai, logging
+import logging
 from flask import Flask, request, make_response
 import json
 
@@ -11,19 +11,18 @@ logging.basicConfig(level=logging.DEBUG)
 def resp():
     event = request.json
     if 'request' in event and 'original_utterance' in event['request'] and len(event['request']['original_utterance']) > 0:
-        text = request.json['request']['command']
-        response_text = f'Вы сказали {text}'
-        response = {
-            "response": {
-                "text": response_text,
-                "end_session": False
+        user_id = event['session']['session_id']
+        user_input = event['request']['original_utterance']
+        response_text = "Вы сказали" + user_input
+        return {
+            'version': event['version'],
+            'session': event['session'],
+            'response': {
+                'text': response_text,
+                'tts': response_text,
+                'end_session': False
             },
-            "version": "1.0",
-            "session": event['session']
         }
-
-        return make_response(response, 200)
-    
     else:
         # If no input was provided, just return a welcome message
         return {
@@ -36,24 +35,5 @@ def resp():
             },
         }
 
-@app.route('/', methods=['GET'])
-def hw():
-    return "Hello, World!"
 
 app.run(debug=True, host='0.0.0.0', port=5000, ssl_context="adhoc")
-
-
-# Set up your OpenAI API credentials
-openai.api_key = "YOUR_API_KEY"
-
-def generate_response(prompt):
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        max_tokens=150,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-
-    return response.choices[0].text.strip()
--- server.py
+++ server.py
@@ -11,3 +11,20 @@ def foo():
 
 if __name__ == '__main__':
    app.run()
+
+
+
+# Set up your OpenAI API credentials
+openai.api_key = "YOUR_API_KEY"
+
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    return response.choices[0].text.strip()
