50a0bc840c11e56c5633374c271902ec30c835d2
Nikolas Miroshnikov <shoveit808@gmail.com>
2023-03-11 12:08:26 +0300

    Update

--- app.py
+++ app.py
@@ -35,5 +35,5 @@ def resp():
             },
         }
 
-
-app.run(debug=True, host='0.0.0.0', port=5000, ssl_context="adhoc")
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context="adhoc")
