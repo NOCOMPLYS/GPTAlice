bec3aae9c1afbbd48c44bc0245d2001e1e10bcc0
NOCOMPLYS <74566080+NOCOMPLYS@users.noreply.github.com>
2023-03-11 17:47:40 +0300

    Added certificates

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def resp():
         }
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context="adhoc")
+    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=('cert.pem', 'key.pem'))
